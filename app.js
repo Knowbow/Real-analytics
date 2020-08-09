39506da31ed941334022f0915e935caa5bd1dfad
Tony Wu <knowbow@gmail.com>
2020-08-09 14:41:30 -0400

    init routes

--- app.js
+++ app.js
@@ -4,11 +4,21 @@ const express = require("express");
 const app = express();
 const db = require("./config/keys").mongoURI;
 mongoose
-  .connect(db, { useNewUrlParser: true })
+  .connect(db, { 
+      useNewUrlParser: true,
+    useUnifiedTopology:true, })
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch((err) => console.log(err));
 
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
+const bodyParser = require("body-parser");
+
 app.get("/", (req, res) => res.send("Hello Worlds"));
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 const port = process.env.PORT || 5000;
 
