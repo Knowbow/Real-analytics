5fed02e27a7f99111c32d8e5bb9744eecd0b9725
Tony Wu <knowbow@gmail.com>
2020-08-11 22:26:49 -0400

    heroku

--- app.js
+++ app.js
@@ -10,7 +10,8 @@ mongoose
   .connect(db, { 
       useNewUrlParser: true,
     useUnifiedTopology:true, })
-  
+  .then(() => console.log("Connected to MongoDB successfully"))
+  .catch((err) => console.log(err));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
