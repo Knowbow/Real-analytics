e94ec0b93237a23814dfb5ef00bea435d504b801
Tony Wu <knowbow@gmail.com>
2020-08-11 22:12:51 -0400

    heroku

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const express = require("express");
 
 const app = express();
 const db = require("./config/keys").mongoURI;
+const users = require("./routes/api/users");
+const bodyParser = require("body-parser");
 mongoose
   .connect(db, { 
       useNewUrlParser: true,
@@ -11,8 +13,6 @@ mongoose
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch((err) => console.log(err));
 
-const users = require("./routes/api/users");
-const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
