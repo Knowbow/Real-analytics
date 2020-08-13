e37cf72e59b0c1060ef0e6ea9857a0c0fd497547
Tony Wu <knowbow@gmail.com>
2020-08-12 22:40:15 -0400

    login nav css

--- frontend/src/components/address_search_bar.js
+++ frontend/src/components/address_search_bar.js
@@ -22,11 +22,7 @@ export default function AddressSearchBar(props) {
     };
 
     return (
-<<<<<<< HEAD
         <div>
-=======
-        <div className='search'>
->>>>>>> d0b702e091630657172e77380d269601ba1bd9ed
             <PlacesAutocomplete value={address} onChange={setAddress} onSelect={handleSelect} >
                 {({ getInputProps }) => (
                     <div>
--- frontend/src/components/session/signup_form.js
+++ frontend/src/components/session/signup_form.js
@@ -51,39 +51,53 @@ class SignupForm extends React.Component {
 
     render() {
         return (
-            <div className="signup">
-                <form onSubmit={this.handleSubmit}>
-                    <div>
-                        <br />
-                        <input type="text"
-                            value={this.state.email}
-                            onChange={this.update('email')}
-                            placeholder="Email"
-                        />
-                        <br />
-                        <input type="text"
-                            value={this.state.handle}
-                            onChange={this.update('handle')}
-                            placeholder="Handle"
-                        />
-                        <br />
-                        <input type="password"
-                            value={this.state.password}
-                            onChange={this.update('password')}
-                            placeholder="Password"
-                        />
-                        <br />
-                        <input type="password"
-                            value={this.state.password2}
-                            onChange={this.update('password2')}
-                            placeholder="Confirm Password"
-                        />
-                        <br />
-                        <input type="submit" value="Submit" />
-                        {this.renderErrors()}
-                    </div>
-                </form>
+          <div className="session">
+            <div className="home-pic">
+              <img
+                className="r-pic"
+                src="https://i.pinimg.com/originals/df/90/7f/df907ff72483dcee2370f294fc9f03dc.jpg"
+              />
             </div>
+            <form onSubmit={this.handleSubmit}>
+              <div>
+                <br />
+                <input
+                  type="text"
+                  value={this.state.email}
+                  onChange={this.update("email")}
+                  placeholder="Email"
+                  className="session-form-sign"
+                />
+                <br />
+                <input
+                  type="text"
+                  value={this.state.handle}
+                  onChange={this.update("handle")}
+                  placeholder="Handle"
+                  className="session-form-sign"
+                />
+                <br />
+                <input
+                  type="password"
+                  value={this.state.password}
+                  onChange={this.update("password")}
+                  placeholder="Password"
+                  className="session-form-sign"
+                />
+                <br />
+                <input
+                  type="password"
+                  value={this.state.password2}
+                  onChange={this.update("password2")}
+                  placeholder="Confirm Password"
+                  className="session-form-sign"
+                />
+                <br />
+                <input className="sessionbtn" type="submit" value="Submit" />
+                {this.renderErrors()}
+              </div>
+            </form>
+          </div>
         );
     }
 }
